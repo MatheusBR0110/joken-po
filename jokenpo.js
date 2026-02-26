dc59b8bbc0da17dc82d3c13f8f672e1677f305d2
MatheusBR0110 <matheus.fernandes01@escola.pr.gov.br>
2026-02-26 09:04:13 -0300

    Add files via upload

--- jokenpo.js
+++ jokenpo.js
@@ -1,50 +1,30 @@
+let imagensPc = ["pcpedra.png","pcpapel.png","pctesoura.png"];
+let imagensPlayer = ["pedra.png","papel.png","tesoura.png"];
+
 function jogar(){
+    let sorteio  = Math.floor(Math.random()*3);
 
-    if(document.getElementById("pedra").checked == false &&
-        document.getElementById("papel").checked == false &&
-        document.getElementById("tesoura").checked == false){
+    document.getElementById("pc").src = imagensPc[sorteio];
 
-            alert("Selecione uma opção")
-
-
+    if((document.getElementById("pedra").checked == true && sorteio ==0)
+        ||(document.getElementById("papel").checked == true && sorteio ==1)
+        ||(document.getElementById("tesoura").checked == true && sorteio ==2))
+        {
+              document.getElementById("placar").innerHTML = "EMPATE!!!";
+    }
+    else if((document.getElementById("pedra").checked == true && sorteio == 2)
+            ||(document.getElementById("papel").checked == true && sorteio == 0)
+            ||(document.getElementById("tesoura").checked == true && sorteio == 1))
+            {
+                document.getElementById("placar").innerHTML = "VOCÊ GANHOU!";
     }
     else{
-
-        let sorteio = Math.floor(Math.random()*3);
-
-        switch(sorteio){
-
-            case 0:
-                document.getElementById("pc").src = "pcpedra.png";
-                break;
-            case 1:
-                document.getElementById("pc").src = "pcpapel.png";
-                break;
-            case 2:
-                document.getElementById("pc").src = "pctesoura.png";
-                break;
-
-        }
-        if((document.getElementById("pedra").checked == true && sorteio == 0)
-            ||(document.getElementById("papel").checked == true && sorteio == 1)
-        ||(document.getElementById("tesoura").checked == true && sorteio == 2))
-        {
-            document.getElementById("placar").innerHTML = "EMPATE!";
-        }
-        else if((document.getElementById("pedra").checked == true && sorteio == 2)
-            ||(document.getElementById("papel").checked == true && sorteio == 0)
-        ||(document.getElementById("tesoura").checked == true && sorteio == 1))
-        {
-            document.getElementById("placar").innerHTML = "Jogador Venceu";
-        }
-        else{
-            document.getElementById("placar").innerHTML = "Computador Venceu";
-        }
-
-
+        document.getElementById("placar").innerHTML = "GANHOU!!";
     }
-}
+    }
+
+
 function resetar(){
-    document.getElementById("pc").src = "pc.png";
-    document.getElementById("placar").innerHTML = "";
+    document.getElementById("pc").src="pc.png";
+    document.getElementById("placar").innerHTML=" ";
 }
